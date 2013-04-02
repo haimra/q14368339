83bbb68db3b40735fc78ac96094dea1600935e11
unknown <ramanh3@.corp.emc.com>
2013-04-02 15:36:47 +0300

    maven issues

--- src/main/java/org/haim/springframwork/stackoverflow/GenericDao.java
+++ src/main/java/org/haim/springframwork/stackoverflow/GenericDao.java
@@ -3,6 +3,7 @@ package org.haim.springframwork.stackoverflow;
 import org.apache.log4j.Logger;
 
 
+
 public class GenericDao<T> {
 	Logger logger = Logger.getRootLogger();
 	public void create(T record) {
